62bf48357e6253dd4284ad0aaa365f1cee01bded
burakyiImaz <132089341+burakyiImaz@users.noreply.github.com>
2026-04-29 19:31:08 +0000

    levenshtein_distance_optimized function implemented

--- algorithm.py
+++ algorithm.py
@@ -18,3 +18,25 @@ def wagner_fischer_distance(s1: str, s2: str) -> int:
                 matrix[i-1][j-1] +cost
             )
     return matrix[len1][len2]
+
+def levenshtein_distance_optimized(s1: str, s2: str) -> int:
+
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    prev = list(range(len(s1) + 1))
+
+    for j, c2 in enumerate(s2, start=1):
+        curr = [j]
+        for i, c1 in enumerate(s1, start=1):
+            cost = 0 if c1 == c2 else 1
+            curr.append(
+                min(
+                    prev[i] + 1,
+                    curr[i - 1] + 1,
+                    prev[i - 1] + cost,
+                )
+            )
+        prev = curr
+
+    return prev[-1]
